24a0c89d9589a2e8c38de9d21dcbb26d4a37ee3f
c1tr00z <c1tr00z@yandex.ru>
2022-10-16 14:27:15 +0900

    Forgot to add collecting db entry process

--- Source/AssistLibUEAllEditor/Private/AssistLibUEAllEditor.cpp
+++ Source/AssistLibUEAllEditor/Private/AssistLibUEAllEditor.cpp
@@ -5,6 +5,8 @@
 #include "AssistLibUEAllEditorCommands.h"
 #include "Misc/MessageDialog.h"
 #include "ToolMenus.h"
+#include "DB/DBLoader.h"
+#include "UObject/SavePackage.h"
 
 static const FName AssistLibUEAllTabName("AssistLibUEAll");
 
@@ -45,13 +47,63 @@ void FAssistLibUEAllEditorModule::ShutdownModule()
 
 void FAssistLibUEAllEditorModule::PluginButtonClicked()
 {
-	// Put your "OnButtonClicked" stuff here
-	FText DialogText = FText::Format(
-							LOCTEXT("PluginButtonDialogText", "Add code to {0} in {1} to override this button's actions"),
-							FText::FromString(TEXT("FAssistLibUEAllModule::PluginButtonClicked()")),
-							FText::FromString(TEXT("AssistLibUEAll.cpp"))
-					   );
-	FMessageDialog::Open(EAppMsgType::Ok, DialogText);
+	UE_LOG(LogTemp, Warning, TEXT("[Assist Lib DB] Start collection DB Entries"));
+	
+	auto DB = DBLoader::LoadDB();
+
+	if (!IsValid(DB))
+	{
+		UE_LOG(LogTemp, Error, TEXT("[Assist Lib DB] NO DB COLLECTION FOUND"));
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("[Assist Lib DB] DB found: %s"), *DB->GetName());
+
+	if (ObjectLibrary == NULL)
+	{
+		ObjectLibrary = UObjectLibrary::CreateLibrary(UDBEntry::StaticClass(), false, GIsEditor);
+		ObjectLibrary->AddToRoot();
+	}
+	ObjectLibrary->LoadAssetDataFromPath(TEXT("/Game/DB"));
+	ObjectLibrary->LoadAssetsFromAssetData();
+	TArray<FAssetData> AssetDatas;
+	ObjectLibrary->GetAssetDataList(AssetDatas);
+
+	TArray<UDBEntry*> Entries;
+	for (FAssetData AssetData : AssetDatas)
+	{
+		auto Entry = Cast<UDBEntry>(AssetData.GetAsset());
+
+		if (!IsValid(Entry))
+		{
+			continue;
+		}
+
+		UE_LOG(LogTemp, Warning, TEXT("[Assist Lib DB] Collecting %s"), *Entry->GetName());
+		Entries.Add(Entry);
+	}
+
+	UPackage* Package = CreatePackage(TEXT("/Game/DB/DB"));
+	Package->FullyLoad();
+	
+	DB->Entries = Entries;
+	DB->SaveConfig();
+	Package->MarkPackageDirty();
+
+	FString PackageFileName = FPackageName::LongPackageNameToFilename(TEXT("/Game/DB/DB"), FPackageName::GetAssetPackageExtension());
+	FSavePackageArgs SaveArgs = { };
+	auto IsSaved = UPackage::SavePackage(Package, DB, *PackageFileName, SaveArgs);
+	// bool bSaved = UPackage::SavePackage(Package, DB, EObjectFlags::RF_Public | EObjectFlags::RF_Standalone, *PackageFileName, GError, nullptr, true, true, SAVE_NoError);
+
+	UE_LOG(LogTemp, Warning, TEXT("[Assist Lib DB] DB Data Asset saved %b"), IsSaved)
+	
+	// // Put your "OnButtonClicked" stuff here
+	// FText DialogText = FText::Format(
+	// 						LOCTEXT("PluginButtonDialogText", "Add code to {0} in {1} to override this button's actions"),
+	// 						FText::FromString(TEXT("FAssistLibUEAllModule::PluginButtonClicked()")),
+	// 						FText::FromString(TEXT("AssistLibUEAll.cpp"))
+	// 				   );
+	// FMessageDialog::Open(EAppMsgType::Ok, DialogText);
 }
 
 void FAssistLibUEAllEditorModule::RegisterMenus()
